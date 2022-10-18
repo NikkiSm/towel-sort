d725f577a4ce89d8d949d47b7c56a100df8d1c0a
unknown <yablonjkas@gmail.com>
2022-10-19 00:17:14 +0300

    feat:towel-sort

---
+++ src/index.js
@@ -0,0 +1,15 @@
+module.exports = function towelSort (matrix) {
+    let array = [];
+    if (matrix === undefined ) {
+     return [];
+    };
+    for (let i = 0; i < matrix.length; i++)
+for (let j = 0; j < matrix[i].length; j++)
+{ let columnIdx = i % 2 === 0 ? j  : (matrix[i].length- j - 1);
+
+array.push( matrix[i][columnIdx] );
+
+    
+    }
+    return array;
+  }
